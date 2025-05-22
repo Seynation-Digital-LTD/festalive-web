18c74247783cfd020547ab177413b6a06db0f25c
omegasey <seynenga@gmail.com>
2025-05-22 16:41:59 +0300

    Hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-       Festalive
-      </p>
+    
+        <div className="hero">
+          <div className="hero-content">
+            <img src="/assets/festalive.png" alt="Festalive" className="logo"/>
+            <h1>Festalive</h1>
+            <p>One platform to <br>discover events</br> , <br>book tickets</br>, and <br>hire creators.</br></p>
+            <p>Be among the first to acces it.</p>
+          </div>
+          
+        </div>
+        
     </>
-  )
+  );
 }
 
-export default App
+export default App;
